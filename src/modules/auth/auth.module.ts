3acd1e8c6a5f3be5cd438a41fb27d2c1218218e0
hoangphuc <hoangphucnb97@gmail.com>
2022-11-13 15:48:46 +0700

    Use validate method from auth service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [LocalStrategy],
+  providers: [LocalStrategy, AuthService],
 })
 export class AuthModule {}
--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -2,15 +2,18 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { Injectable } from '@nestjs/common';
 import { IUserInfo } from '../user-info.interface';
+import { AuthService } from '../auth.service';
+import { UserUnauthorizedException } from '../../../shares/exceptions/auth.exception';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super();
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'email' });
   }
 
   async validate(username: string, password: string): Promise<IUserInfo> {
-    if (password === '1') return { username, password };
-    return null;
+    const user = await this.authService.validateUserWithUsernameAndPassword(username, password);
+    if (!user) throw new UserUnauthorizedException(`Email and password not correct`);
+    return user;
   }
 }
