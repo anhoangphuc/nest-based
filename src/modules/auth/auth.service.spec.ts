2b25ce0ecc204b73414129da30316a5237ae2b18
hoangphuc <hoangphucnb97@gmail.com>
2022-11-13 16:56:09 +0700

    Add test user login

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,13 +1,24 @@
 import { AuthService } from './auth.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtExpireTime, JwtSecretKey } from '../../shares/constants/auth.constant';
 
 describe(`AuthService`, () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, LocalStrategy],
+      imports: [
+        JwtModule.register({
+          secret: JwtSecretKey,
+          signOptions: {
+            expiresIn: JwtExpireTime,
+          },
+        }),
+      ],
+      providers: [AuthService, LocalStrategy, JwtStrategy],
     }).compile();
 
     service = moduleRef.get<AuthService>(AuthService);
@@ -20,12 +31,18 @@ describe(`AuthService`, () => {
   describe(`validate user`, () => {
     it(`Login success`, async () => {
       const res = await service.validateUserWithUsernameAndPassword('hoangphucnb97@gmail.com', '1');
-      expect(res.username === 'hoangphucnb97@gmail.com' && res.password === '1');
+      expect(res.username === 'hoangphucnb97@gmail.com');
     });
 
     it(`Login with not correct user`, async () => {
       const res = await service.validateUserWithUsernameAndPassword(`hoangphucnb97@gmail.com`, '2');
       expect(res).toBeNull();
     });
+  });
+
+  it(`User login`, async () => {
+    const res = await service.login({ username: 'hoangphucnb97@gmail.com' });
+    expect(res).toBeDefined();
+    expect(res.access_token).toBeDefined();
   });
 });
