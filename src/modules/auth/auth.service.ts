e85510c9c27472c42f02e95b05b58602704e0e5a
hoangphuc <hoangphucnb97@gmail.com>
2022-11-13 19:12:52 +0700

    Add login request and response

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
   async login(user: IUserInfo) {
     const payload = { username: user.username };
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 
