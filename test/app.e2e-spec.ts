746437e72f246136bd0d2da2c4e27308fd0717bf
hoangphuc <hoangphucnb97@gmail.com>
2022-11-17 09:53:58 +0700

    Update tsconfig and relevent import

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { sleep } from '../src/shares/helpers/utils';
 import { JwtExpireTime } from '../src/shares/constants/auth.constant';
