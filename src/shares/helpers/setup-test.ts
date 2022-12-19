fdf3c4c05c292dc5d7f6fcfa361637b3de794d28
hoangphuc <hoangphucnb97@gmail.com>
2022-12-19 11:09:36 +0700

    Add random function to generate email and password

--- src/shares/helpers/setup-test.ts
+++ src/shares/helpers/setup-test.ts
@@ -18,3 +18,18 @@ export const closeMongodConnection = async () => {
     await mongod.stop();
   }
 };
+
+export function randomString(length: number, withNumber: boolean) {
+  const s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  Array.from(Array(length).keys())
+    .map((_) => s.charAt(Math.floor(Math.random() * (withNumber ? s.length : s.length - 10))))
+    .join('');
+}
+
+export function randomEmail(): string {
+  return `${randomString(10, false)}@email.com`;
+}
+
+export function randomPassword(): string {
+  return `${randomString(10, true)}`;
+}
