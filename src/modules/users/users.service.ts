f00f94251d84f39c345798bb20c6768d51e3dfc1
hoangphuc <hoangphucnb97@gmail.com>
2022-12-20 17:24:00 +0700

    Update unit test and update method verify token

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -36,7 +36,7 @@ export class UsersService {
     const user = await this.usersModel.findOne({ email }, {}, { session });
     if (isNullOrUndefined(user)) throw new UserNotFoundException({ email });
     if (user.role === UsersRole.USER_ACTIVATED) throw new UserAlreadyExistException(email);
-    return this.usersModel.findOneAndUpdate({ email }, { isActivated: true }, { session, new: true });
+    return this.usersModel.findOneAndUpdate({ email }, { role: UsersRole.USER_ACTIVATED }, { session, new: true });
   }
 
   async addNewUserWithNewTransaction(registerRequest: RegisterRequestDto): Promise<UsersDocument> {
