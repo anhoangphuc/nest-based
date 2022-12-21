f78a68f237b0af934ba7087fa1c0cc9709d9a1fb
hoangphuc <hoangphucnb97@gmail.com>
2022-12-21 15:25:48 +0700

    Update method get users info

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../../shares/guards/jwt-auth.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UsersInfoResponseDto } from './dto/users-info-response.dto';
+import { UserNotFoundException } from '../../shares/exceptions/users.exception';
+import { plainToInstance } from 'class-transformer';
 
 @Controller('users')
 @ApiBearerAuth()
@@ -10,7 +13,20 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('')
-  async getListUsers() {
-    return await this.usersService.getListOfUsers({}, null);
+  @ApiOperation({
+    operationId: 'user-get-info',
+    description: 'User get his information',
+    summary: 'User get his information',
+  })
+  @ApiResponse({
+    type: UsersInfoResponseDto,
+    status: HttpStatus.OK,
+    description: 'Successful',
+  })
+  async getUserInfo(@Request() request): Promise<UsersInfoResponseDto> {
+    const { user } = request;
+    const foundUser = await this.usersService.getListOfUsers({ email: [user.email] }, null);
+    if (foundUser.length === 0) throw new UserNotFoundException({ email: user.email });
+    return plainToInstance(UsersInfoResponseDto, foundUser[0]);
   }
 }
