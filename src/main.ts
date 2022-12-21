bca93676562880f1b71a0957930f681fdb4794b5
hoangphuc <hoangphucnb97@gmail.com>
2022-12-21 16:10:11 +0700

    Process only whitelist fields dto

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await setupSwagger(app);
   await app.listen(3000);
