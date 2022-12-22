d9f20542d6bd06eba22841d532df95a52adedd8c
hoangphuc <hoangphucnb97@gmail.com>
2022-12-22 10:43:54 +0700

    Add test verify signature

--- src/modules/account-signature/eth-signature.service.spec.ts
+++ src/modules/account-signature/eth-signature.service.spec.ts
@@ -2,6 +2,8 @@ import { EthSignatureService } from './eth-signature.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignatureComponentService } from './signature-component.service';
 import { CoreModule } from '../core/core.module';
+import { ethers, TypedDataField } from 'ethers';
+import { sign } from 'crypto';
 
 describe(`EthService`, () => {
   let service: EthSignatureService;
@@ -15,5 +17,26 @@ describe(`EthService`, () => {
 
   it(`Should be defined`, () => {
     expect(service).toBeDefined();
+  });
+
+  it(`Verify signature success`, async () => {
+    const signer = ethers.Wallet.createRandom();
+    const chainId = 1;
+    const domain = {
+      name: 'nest-based',
+      chainId,
+    };
+    const types = {
+      Request: [{ name: 'request', type: 'string' }],
+    };
+
+    const values = {
+      request: 'link_address',
+    };
+
+    const signature = await signer._signTypedData(domain, types, values);
+
+    const verifiedAddress = await service.verifyEthLinkAddressSignature(chainId, signature);
+    expect(verifiedAddress).toEqual(signer.address);
   });
 });
